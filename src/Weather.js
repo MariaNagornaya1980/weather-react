6abe12e8914859928c671cf69077c5aeda8eed64
MariaNagornaya1980 <mariawright205@gmail.com>
2024-10-04 17:31:00 -0700

    added a loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,25 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
-export default function Weaher() {
+export default function Weaher(props) {
   function handleResponse(response) {
-    alert(`The weather in New York is ${response.data.main.temp}℃`);
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}℃`
+    );
   }
   let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 }
